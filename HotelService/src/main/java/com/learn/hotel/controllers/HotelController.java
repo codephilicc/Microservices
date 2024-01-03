55cd512841fa0012b7b061983c4c9915715d276f
Rahul <rahulkr.vk@gmail.com>
2024-01-03 22:13:51 +0530

    adding @pathvariable in hotelcontroller

--- HotelService/src/main/java/com/learn/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/learn/hotel/controllers/HotelController.java
@@ -1,12 +1,12 @@
 package com.learn.hotel.controllers;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,23 +21,23 @@ public class HotelController {
 
 	@Autowired
 	private HotelService hotelService;
-	
-	//	create
+
+	// create
 	@PostMapping
-	public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
+	public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
 	}
-	
-	//	get single
+
+	// get single
 	@GetMapping("/{hotelId}")
-	public ResponseEntity<Hotel> getSingle(String hotelId){
+	public ResponseEntity<Hotel> getSingle(@PathVariable String hotelId){
 		return ResponseEntity.status(HttpStatus.OK).body(hotelService.getString(hotelId));
 	}
-	
-	//get all	
+
+	// get all
 	@GetMapping
-	public ResponseEntity<List<Hotel>> getAll(){
+	public ResponseEntity<List<Hotel>> getAll() {
 		return ResponseEntity.ok(hotelService.getAll());
 	}
-	
+
 }
