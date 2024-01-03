59a71a803816ed47b2a8e428bf4f749d907d4cb1
Rahul <rahulkr.vk@gmail.com>
2024-01-03 22:51:49 +0530

    calling two microservice together

--- UserService/src/main/java/com/learn/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/learn/user/service/services/impl/UserServiceImpl.java
@@ -9,35 +9,36 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.learn.user.service.entities.Hotel;
 import com.learn.user.service.entities.Rating;
 import com.learn.user.service.entities.User;
 import com.learn.user.service.exception.ResourceNotFoundexception;
 import com.learn.user.service.repositories.UserRepository;
 import com.learn.user.service.services.UserService;
 
-
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
-	
+
 	@Override
 	public User saveUser(User user) {
-		//generate unique userid		
+		// generate unique userid
 		String randomUserId = UUID.randomUUID().toString();
 		user.setUserId(randomUserId);
 		return userRepository.save(user);
 	}
-	
+
 	@Override
 	public List<User> getAllUser() {
 		return userRepository.findAll();
@@ -45,23 +46,35 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUser(String userId) {
-		//get user from database with the help of user repository		
-		User user =  userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundexception("User with given id is not found on server !! : "+userId));
-		//fetch rating of the above user from RATING SERVICE		
-		//http://localhost:8083/ratings/users/86fd4b90-0e6b-4c14-885b-3fb4011a9428		
+		// get user from database with the help of user repository
+		User user = userRepository.findById(userId).orElseThrow(
+				() -> new ResourceNotFoundexception("User with given id is not found on server !! : " + userId));
+		// fetch rating of the above user from RATING SERVICE
+		// http://localhost:8083/ratings/users/86fd4b90-0e6b-4c14-885b-3fb4011a9428
+
+		Rating[] ratingsOfUser = restTemplate
+				.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), Rating[].class);
+		logger.info("{}", ratingsOfUser);
 		
-		ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
-		logger.info("{}" ,  ratingsOfUser);
+		List<Rating> ratings = Arrays.stream(ratingsOfUser).toList();
 		
-		List<Rating> ratingList = ratingsOfUser.stream().map(rating ->{
-			//api call to hotel service to get the hotel
-			
-			
+		List<Rating> ratingList = ratings.stream().map(rating -> {
+			// api call to hotel service to get the hotel
+			// http://localhost:8082/hotels/dcdeeb03-03d4-49e8-87d5-2da466789430
+
+			ResponseEntity<Hotel> forEntity = restTemplate
+					.getForEntity("http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+			Hotel hotel = forEntity.getBody();
+			logger.info("{}", forEntity.getStatusCode());
+
+			//set the hotel to rating			
+			rating.setHotel(hotel);
+			//return the rating			
 			return rating;
 		}).collect(Collectors.toList());
-		
-		user.setRatings(ratingsOfUser);
-		
+
+		user.setRatings(ratingList);
+
 		return user;
 	}
 
