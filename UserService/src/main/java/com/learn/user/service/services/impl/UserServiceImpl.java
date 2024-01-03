453a9c42ef76d301edc2bf6f70404defaa3b06b6
Rahul <rahulkr.vk@gmail.com>
2024-01-03 20:19:24 +0530

    talk user and rating microservices

--- UserService/src/main/java/com/learn/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/learn/user/service/services/impl/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.learn.user.service.services.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.learn.user.service.entities.Rating;
 import com.learn.user.service.entities.User;
 import com.learn.user.service.exception.ResourceNotFoundexception;
 import com.learn.user.service.repositories.UserRepository;
@@ -17,6 +23,11 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 	
 	@Override
 	public User saveUser(User user) {
@@ -33,7 +44,15 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public User getUser(String userId) {
-		return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundexception("User with given id is not found on server !! : "+userId));
+		//get user from database with the help of user repository		
+		User user =  userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundexception("User with given id is not found on server !! : "+userId));
+		//fetch rating of the above user from RATING SERVICE		
+		//http://localhost:8083/ratings/users/86fd4b90-0e6b-4c14-885b-3fb4011a9428		
+		
+		ArrayList<Rating> forObject = restTemplate.getForObject("http://localhost:8083/ratings/users/86fd4b90-0e6b-4c14-885b-3fb4011a9428", ArrayList.class);
+		logger.info("{}" ,  forObject);
+		
+		return user;
 	}
 
 }
